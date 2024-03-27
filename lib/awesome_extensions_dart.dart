509a4af69382906ed57d97a16698eca61862b452
Altynbek Aidarbekov <altynbek.ktl@gmail.com>
2024-03-28 00:12:33 +0500

    chore: Update import statements in awesome_extensions_dart.dart and awesome_extensions_flutter.dart

--- lib/awesome_extensions_dart.dart
+++ lib/awesome_extensions_dart.dart
@@ -1,3 +1,1 @@
-export 'src/dart/date_extensions/date_extension.dart';
-export 'src/dart/number_extension.dart';
-export 'src/dart/string_extension.dart';
+export 'src/dart/_src.dart';
---
+++ lib/src/dart/_src.dart
@@ -0,0 +1,3 @@
+export 'date_extensions/date_extension.dart';
+export 'number_extension.dart';
+export 'string_extension.dart';
